d61a4b0fa2f4085174604e28d10c472a1f1cedbb
TheNiteCoder <daleysamuel0208@gmail.com>
2019-09-28 18:47:22 -0400

    Updating

--- 2.0/forum.py
+++ 2.0/forum.py
@@ -10,7 +10,7 @@ class ConstructError(BaseException):
     def __str__(self):
         return self.__msg__
 
-class ForumObject:
+class ForumSelectorObject:
     def __init__(self, selectors=dict()):
         self.__selectors__ = selectors
         # self.selector = dict()
@@ -22,9 +22,14 @@ class ForumObject:
         if not name in self.__selectors__.keys():
             return None
         return self.__selectors__[name]
-        
 
-class Section(ForumObject):
+class ForumPageObject:
+    def __init__(self, url):
+        self.__url__ = url
+    def url(self):
+        return self.__url__
+
+class Section(ForumSelectorObject):
     def __init__(self):
         super().__init__({'global' : 'div:is(.forabg, .forumbg)'})
         self.link = ''
@@ -47,22 +52,22 @@ class Section(ForumObject):
                 try:
                     fv.construct(str(match))
                     self.items.append(fv)
-                except ConstructError:
-                    raise ConstructError('fv.construct threw ContructError: ')
+                except ConstructError as ce:
+                    raise ConstructError('fv.construct threw ContructError: ' + ce.msg)
             for match in parser.select(TopicView().selector('section')):
                 tv = TopicView()
                 try:
                     tv.contruct(str(match))
                     self.items.append(tv)
-                except ConstructError:
-                    raise ConstructError('tv.construct threw ConstructError: ')
+                except ConstructError as ce:
+                    raise ConstructError('tv.construct threw ConstructError: ' + ce.msg)
 
 
-class TopicView(ForumObject):
+class TopicView(ForumSelectorObject):
     def __init__(self):
         super().__init__({'global' : 'div:is(.forabg, .forumbg) ul li:has(a:is(.topictitle)):not(.row)',
                           'section' : 'ul li:has(a:is(.topictitle):not(.row))'})
-        self.link = ''
+        self.url = ''
         self.title = ''
     def contruct(self, html):
         if html == None:
@@ -73,27 +78,41 @@ class TopicView(ForumObject):
             raise ConstructError('topictitle == None')
         if not topictitle.has_attr('href'):
             raise ConstructError('not topictitle.has_attr(\'href\')')
-        self.link = topictitle['href']
+        self.url = topictitle['href']
         i = topictitle.find('i')
         if i != None:
             i.decompose()
         self.title = topictitle.text
         self.title = self.title.strip()
 
-class ForumView(ForumObject):
+class ForumView(ForumSelectorObject):
     def __init__(self):
         super().__init__({'global' : 'div:is(.forabg, .forumbg) ul:is(.topiclist) li:is(.row) dl:is(.row-item) dt div:is(.list-inner)',
                           'section' : 'ul:is(.topiclist) li:is(.row) dl:is(.row-item) dt div:is(.list-inner)'})
         self.title = ''
-        self.link = ''
+        self.url = ''
     def construct(self, html):
         p1 = requestor.BeautifulSoup(html, 'html.parser')
         title_link = p1.select_one('a:is(.forumtitle)')
         if title_link == None:
             raise ConstructError('title_link == None')
         if title_link.has_attr('href'):
-            self.link = title_link['href']
+            self.url = title_link['href']
         self.title = title_link.text
         self.title = self.title.strip()
-
+        
+class ForumPage(ForumPageObject):
+    def __init__(self, url):
+        super().__init__(url)
+        self.title = ''
+        self.sections = []
+    def construct(self, req, pages=1):
+        htmls = req.request(self.url(), pages=pages)
+        self.title = str(requestor.BeautifulSoup(htmls[0], 'html.parser').find('title').text)
+        for html in htmls:
+            p1 = requestor.BeautifulSoup(html, 'html.parser')
+            for match in p1.select(Section().selector()):
+                section = Section()
+                section.construct(str(match), construct_with_topic_and_forum_views=True)
+                self.sections.append(section)
 
--- 2.0/try.py
+++ 2.0/try.py
@@ -1,2 +1,19 @@
 
+import forum 
+import requestor
+import curses
 
+session = requestor.Session()
+session.login('https://forum.mxlinux.org/login.php', 'TheNiteCoder', '02commandercody08')
+
+req = requestor.Requestor(session=session)
+
+f = forum.ForumPage('https://forum.mxlinux.org')
+f.construct(req)
+
+print('=== ' + f.title + ' ===')
+
+for section in f.sections:
+    for item in section.items:
+        if type(item) is forum.TopicView:
+            print(item.title)
